7db3bdffe7435005bc8b42fb60f635a2c5e601a7
CoopTRUE <coopdog4545@gmail.com>
2021-06-30 14:47:19 -0700

    added support for url image uploading

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO
+from requests import get
 
 ACCEPTABLE_FILENAMES = ['png', 'jpg', 'jpeg', 'gif']
 
@@ -13,11 +14,15 @@ def main_page():
 
 @app.route('/',  methods=['POST'])
 def upload_file():
-    file = request.files.get('file')
-    if file and file.filename.rsplit('.', 1)[1] in ACCEPTABLE_FILENAMES:
-        socketio.emit('refresh')
-        file.save('static/image')
-        print('yep')
+    if file := request.files.get('file'):
+        if file.filename.rsplit('.', 1)[1] in ACCEPTABLE_FILENAMES:
+            socketio.emit('refresh')
+            file.save('static/image')
+    elif url := request.form.get('url'):
+        if url.rsplit('.', 1)[1] in ACCEPTABLE_FILENAMES:
+            r = get(url)
+            with open('static/image', 'wb') as file:
+                file.write(r.content)
     return redirect('..')
 
 
